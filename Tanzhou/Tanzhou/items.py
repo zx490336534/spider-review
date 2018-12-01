c3831fdfb35246dc9cc82758cd6f2d5cbe169cc7
zhongxin <490336534@qq.com>
2018-12-01 17:02:33 +0800

    爬取潭州教育

---
+++ Tanzhou/Tanzhou/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class TanzhouItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    #课程金额
+    money = scrapy.Field()
+    #课程名字
+    title = scrapy.Field()
---
+++ Tanzhou/Tanzhou/spiders/tz.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import time
+import scrapy
+from Tanzhou.items import TanzhouItem
+
+class TzSpider(scrapy.Spider):
+    name = 'tz' #爬虫名字
+    allowed_domains = ['tanzhouedu.com'] #限制爬虫范围
+    start_urls = ['http://www.tanzhouedu.com/mall/course/initAllCourse'] #请求url的第一个链接
+    offset = 0
+
+    def parse(self, response):
+        item = TanzhouItem()
+        node_list = response.xpath('//div[@id="newCourse"]/div/div/ul/li')
+        for node in node_list:
+            item['money'] = node.xpath('./div/span/text()').extract_first()
+            item['title'] = node.xpath('./a/@title').extract_first()
+            yield item
+        if not node_list:
+            return
+        self.offset += 20
+        yield scrapy.Request(url="http://www.tanzhouedu.com/mall/course/initAllCourse?params.offset="+str(self.offset)+"&params.num=20&keyword=&_="+str(int(time.time()*1000)), callback=self.parse)
+        #url会被传送给Scheduler【调度器】整理入列
