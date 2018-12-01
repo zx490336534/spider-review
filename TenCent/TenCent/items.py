98db0b80ef0a58b76048d6dc27771f72389916ab
zhongxin <490336534@qq.com>
2018-12-01 17:03:17 +0800

    爬取腾讯招聘并存储至一个文件中

---
+++ TenCent/TenCent/items.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class TencentItem(scrapy.Item):
+    #职位名称
+    position_name = scrapy.Field()
+    #详情链接
+    position_link = scrapy.Field()
+    #职位类别
+    position_type = scrapy.Field()
+    #招聘人数
+    people_number = scrapy.Field()
+    #工作地点
+    work_location = scrapy.Field()
+    #招聘时间
+    publish_times = scrapy.Field()
+    #工作职责
+    position_zhize = scrapy.Field()
+    #工作要求
+    position_yaoqiu = scrapy.Field()
---
+++ TenCent/TenCent/spiders/tc.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from TenCent.items import TencentItem
+
+
+class TcSpider(scrapy.Spider):
+    name = 'tc'
+    allowed_domains = ['hr.tencent.com']
+    base_url = "https://hr.tencent.com/"
+    start_urls = ['https://hr.tencent.com/position.php']
+
+    def parse(self, response):
+        node_list = response.xpath("//tr[@class='even']|//tr[@class='odd']")
+        next_page = response.xpath('//a[@id="next"]/@href').extract_first()
+
+        for node in node_list:
+            item = TencentItem()
+            item['position_name'] = node.xpath("./td[1]/a/text()").extract_first()
+            item['position_link'] = node.xpath("./td[1]/a/@href").extract_first()
+            item['position_type'] = node.xpath("./td[2]/text()").extract_first()
+            item['people_number'] = node.xpath("./td[3]/text()").extract_first()
+            item['work_location'] = node.xpath("./td[4]/text()").extract_first()
+            item['publish_times'] = node.xpath("./td[5]/text()").extract_first()
+            yield scrapy.Request(url=self.base_url+item['position_link'],callback=self.detail,meta={'item':item})
+        yield scrapy.Request(url=self.base_url+next_page,callback=self.parse)
+
+    def detail(self,response):
+        item = response.meta["item"]
+        item['position_zhize'] = "".join(response.xpath('//ul[@class="squareli"]')[0].xpath("./li/text()").extract())
+        item['position_yaoqiu'] = "".join(response.xpath('//ul[@class="squareli"]')[1].xpath("./li/text()").extract())
+        yield item
